d187313eb6097a5f8dde224b589d81ad081891ee
Keranod <konrad.konkel@wp.pl>
2023-10-23 13:30:33 +0100

    Added eslint

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,7 +2,7 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (request, response) => {
-    Blog
+  Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
@@ -10,19 +10,19 @@ blogsRouter.get('/', (request, response) => {
 })
 
 blogsRouter.post('/', (request, response) => {
-    const body = request.body
-  
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes,
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
   })
 
   blog.save()
-  .then(savedBlog => {
-    response.json(savedBlog)
-  })
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
 })
 
 module.exports = blogsRouter
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,20 +1,33 @@
 const dummy = (blogs) => {
-    if (blogs) {
-        return 1
-    }
+  if (blogs) {
+    return 1
   }
-  
-const totalLikes = (blogs) => {
-    let sumOfLikes = 0
-    
-    blogs.forEach( (blog) => {
-        sumOfLikes += blog.likes
-    })
-
-    return sumOfLikes
 }
 
-  module.exports = {
-    dummy,
-    totalLikes
-  }
+const totalLikes = (blogs) => {
+  let sumOfLikes = 0
+
+  blogs.forEach((blog) => {
+    sumOfLikes += blog.likes
+  })
+
+  return sumOfLikes
+}
+
+const favoriteBlog = (blogs) => {
+  let blogWithMostLikes = blogs[0]
+
+  blogs.forEach((blog) => {
+    if (blog.like > blogWithMostLikes.likes) {
+      blogWithMostLikes = blog
+    }
+  })
+
+  return blogWithMostLikes
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
