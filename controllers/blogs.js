d10818ea1865ad9c523f98b56fe21f2fea425edd
Keranod <konrad.konkel@wp.pl>
2023-10-26 13:36:23 +0100

    4.17 take3

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -31,7 +31,7 @@ blogsRouter.post('/', async (request, response) => {
         author: body.author,
         url: body.url,
         likes: body.likes,
-        id: user.id
+        user: user.id
     })
 
     const savedBlog = await blog.save()
