79dfe2ececa94693f543208b5b912b21decb26e6
Keranod <konrad.konkel@wp.pl>
2023-10-26 14:23:40 +0100

    4.19 take1

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -9,8 +10,22 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     if (!body.title) {
         return response.status(400).end()
@@ -23,8 +38,6 @@ blogsRouter.post('/', async (request, response) => {
     if (!body.likes) {
         body.likes = 0
     }
-
-    const user = await User.findById(body.userId)
 
     const blog = new Blog({
         title: body.title,
