9081f27e34edb226cf4ea8826385b3ec96a518ed
Keranod <konrad.konkel@wp.pl>
2023-10-26 10:42:53 +0100

    4.15 take2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
@@ -12,6 +13,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
