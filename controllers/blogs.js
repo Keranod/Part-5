f28b4bd89e5b69b672b8329b1a7e0a14cec7b5b8
Keranod <konrad.konkel@wp.pl>
2023-10-27 08:15:16 +0100

    4.22 take1

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -13,11 +13,13 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: 'token invalid' })
-    }
-    const user = await User.findById(decodedToken.id)
+    // const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    // if (!decodedToken.id) {
+    //     return response.status(401).json({ error: 'token invalid' })
+    // }
+    // const user = await User.findById(decodedToken.id)
+
+    const user = request.user
 
     if (!body.title) {
         return response.status(400).end()
@@ -47,11 +49,13 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: 'token invalid' })
-    }
-    const userId = decodedToken.id
+    // const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    // if (!decodedToken.id) {
+    //     return response.status(401).json({ error: 'token invalid' })
+    // }
+
+    const user = request.user
+    const userId = user.id
     const blog = await Blog.findById(request.params.id)
 
     if (blog.user.toString() !== userId.toString()) {
