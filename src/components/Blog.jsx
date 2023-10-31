e5eeffe0ecbae8ec1eb769f06d506e59f17d37de
Keranod <konrad.konkel@wp.pl>
2023-10-31 14:51:21 +0000

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails] = useState('none')
+
+  const toggleViewDetails = () => {
+    if (viewDetails === 'none') {
+      setViewDetails('')
+    } else {
+      setViewDetails('none')
+    }
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return(
+  <div style={blogStyle}>
+    <div>
+      {blog.title} {blog.author}
+      <button onClick={toggleViewDetails}>view</button>
+    </div>
+    <div style={{ display: viewDetails }}>
+      {blog.url}<br/>
+      likes {blog.likes}
+      <button onClick={null}>like</button><br/>
+      {blog.user.name}
+    </div>
   </div>  
-)
+)}
+
 
 export default Blog
